b8ff56d332b73d0a426a78335b95c65b483e0a4b
Igbathscaller <Igbathscaller@gmail.com>
2021-01-06 13:16:35 -0500

    intialize

---
+++ Node.java
@@ -0,0 +1,26 @@
+public class Node {
+    public Node(String value){/*create a constructor*/
+    
+    }
+    
+    private String data;
+    
+    private Node next,prev;
+    //write get/set methods for all three instance variables.
+
+    public Node next(){
+        return next;
+    }
+
+    public Node prev(){
+        return prev;
+    }
+
+    public void setNext(){
+
+    }
+
+    public void setPrev(){
+        
+    }
+}
